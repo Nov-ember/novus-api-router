fcce78f76db393d46584560086ad8e05c048b04d
Nov-ember <15804035913@163.com>
2026-09-15 20:48:58 +0800

    chore: initialize API router project

---
+++ src/main/java/com/example/novusapirouter/NovusApiRouterApplication.java
@@ -0,0 +1,13 @@
+package com.example.novusapirouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovusApiRouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovusApiRouterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/novusapirouter/NovusApiRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.novusapirouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovusApiRouterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
